b29d007b2c34495d05374213116828bb4d55a868
R. Fariz <farizramazanov38@gmail.com>
2023-03-28 19:52:18 +0400

    feat: add popups

--- assets/js/administrator.js
+++ assets/js/administrator.js
@@ -1,24 +1,56 @@
-const popup = document.querySelector('.administrator-popup');
-const popupContent = document.querySelector('.administrator-popup-content');
-const closeBtn = document.querySelector('.administrator-close-btn');
-const popupTriggers = document.querySelectorAll('.adminitrator-table-item__popup');
+const popup = document.querySelector(".administrator-popup");
+const popupContent = document.querySelector(".administrator-popup-content");
+const closeBtn = document.querySelector(".administrator-close-btn");
+const popupTriggers = document.querySelectorAll(
+  ".adminitrator-table-item__popup"
+);
 
-popupTriggers.forEach(trigger => {
-  trigger.addEventListener('click', e => {
+popupTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", (e) => {
     e.preventDefault();
-    popup.classList.add('popup-transition');
-    popup.style.display = 'block';
+    popup.classList.add("popup-transition");
+    popup.style.display = "block";
   });
 });
 
-closeBtn.addEventListener('click', () => {
-  popup.classList.remove('popup-transition');
-  popup.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+  popup.classList.remove("popup-transition");
+  popup.style.display = "none";
 });
 
-popup.addEventListener('click', e => {
+popup.addEventListener("click", (e) => {
   if (e.target === popup) {
-    popup.classList.remove('popup-transition');
-    popup.style.display = 'none';
+    popup.classList.remove("popup-transition");
+    popup.style.display = "none";
   }
+});
+
+const officiantBtn = document.getElementById("officiant-btn");
+const orderBtn = document.getElementById("order-btn");
+const officiantPopupContainer = document.getElementById(
+  "officiant-popup-container"
+);
+const orderPopupContainer = document.getElementById("order-popup-container");
+
+const officiantPopupCloseBtn = document.getElementById(
+  "officiant-popup-close-btn"
+);
+
+const orderPopupCloseBtn = document.getElementById("order-popup-close-btn");
+
+officiantBtn.addEventListener("click", () => {
+  officiantPopupContainer.style.display = "flex";
+});
+
+orderBtn.addEventListener("click", () => {
+  orderPopupContainer.style.display = "flex";
+});
+
+// Add event listeners to the close buttons for each popup
+officiantPopupCloseBtn.addEventListener("click", () => {
+  officiantPopupContainer.style.display = "none";
+});
+
+orderPopupCloseBtn.addEventListener("click", () => {
+  orderPopupContainer.style.display = "none";
 });
